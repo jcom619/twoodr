98a3cc5ea1646dcb11d0cbe3c4dd45cb9b477c4d
Jcom619 <joshuacompton@my.smccd.edu>
2021-01-28 15:58:51 -0800

    first update

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-document.querySelector('#submit-twood').addEventListener('click', () => {
-  const newText = document.querySelector('#twood-input').value
-  // document.querySelector('.twood').innerText = newText
-  document.querySelector('#twood-input').value = ''
-
-  const newTwood = document.createElement('div')
-  document.querySelector('.twoods-container').append(newTwood)
-  // newTwood.classList.add('twood')
-  // newTwood.innerText = newText
 
 
-})
-
-setTimeout(() => {
-  document.querySelector('.cutoff').classList.remove('hidden')  
-}, 200000)
+function newTwood() {
+  var newText = document.getElementById("twood-input").value;
+  document.getElementById("txtDisplay").innerText = newText;
+}
