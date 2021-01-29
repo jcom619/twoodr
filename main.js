046e603465600b7c29a9151bf7470bbd01e66f31
Jcom619 <joshuacompton@my.smccd.edu>
2021-01-28 16:53:11 -0800

    v3

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-
-
 function newTwood() {
   var newText = document.getElementById("twood-input").value;
   document.getElementById("txtDisplay").innerText = newText;
 }
+
+
+function timeCutOff() {
+  setTimeout(function () {
+    const element = document.getElementById('done')
+    element.classList.remove('hidden')
+  }, 3000);
+}
+
+timeCutOff();
